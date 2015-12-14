afa005166b267615b64d301543d240b76bf50da3
terbatasFG <am_ad_ya_sin@yahoo.co.id>
2015-12-14 13:05:25 +0700

    yasin
    tambah file lathian1

---
+++ latihan1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Latihan 1</title>
+</head>
+
+<body>
+Haloooo.....
+<?PHP
+print date("\T\a\\n\g\g\a\l d-M-Y \J\a\m H:i:s");
+echo "<br>";
+date_default_timezone_set("Asia/Jakarta"); 
+echo time();
+
+?><!-- <meta http-equiv="refresh" content="1" /> -->
+
+
+</body>
+</html>
